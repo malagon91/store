c3f424a7fe4ad67fc794efd08e3a7b240afc17c3
Unknown <miguelmalagon@outlook.com>
2018-06-12 08:25:40 -0500

    add migrations

---
+++ database/migrations/2018_06_12_131938_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('representative_id')->unsigned()->index();
+            $table->decimal('amount',9,2);
+            $table->date('due_date');
+            $table->string('customer_name');
+            $table->string('wtp');
+            $table->string('notes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
